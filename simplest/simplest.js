fa74d2008eae08c46dd764d37b8aaa35a86d6f26
Nick Cottrell <nick.a.cottrell@gmail.com>
2013-09-19 01:04:52 -0700

    basic controls seem to be working

--- simplest/simplest.js
+++ simplest/simplest.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
 
 		var searchString = document.location.search;
-		var slidenum = searchString.substring(1);
-		var images=$('#images img');
-		if (slidenum == undefined ){
-			var index=0;
-		} else {
-		    slidenum = slidenum - 1;	
-			var index=slidenum;
-		}
-		images.eq(index).fadeIn(300);
-		
-		setInterval(playSlides,3400);
-		
-		//playSlides();
+		var currenturl = searchString.split("&");
+		var slide_control = currenturl[0];
+		slide_control = slide_control.split("=");
+		slide_control = slide_control[1];
+		var play_control = currenturl[1];
+		play_control = play_control.split("=");
+		play_control = play_control[1];		
 
+		var images=$('#images img');
+
+		if (slide_control === undefined ) {
+		var index=0;
+		} else {
+ 		var index=slide_control-1;
+		}
+	
+		images.eq(index).show();
+
+		if (play_control === undefined ) {
+		setInterval(playSlides,1400);
+		} else if (play_control == "play"){
+		setInterval(playSlides,1400);
+		} else if (play_control == "pause"){
+			// do nothing
+		}
+		
 		function playSlides(){
-			images.eq(index).fadeIn(100).delay(3000).fadeOut(300);
+			images.eq(index).fadeIn(100).delay(1000).fadeOut(300);
 			index=(index+1) % images.length;
-			var nextslide = index;
-			var currenturl = document.location.href;
-			currenturl = currenturl.split("?");
-			currentslide = currenturl[0];
-			
-			window.history.pushState("?" + currentslide, "Title", "?" + nextslide);			
+			window.history.pushState("?slide=" + index-1 + "&control=play", "Title", "?slide=" + index + "&control=play");
 		};
 		
 		$('.NEXT').click(function(){
@@ -33,15 +40,22 @@ $(document).ready(function() {
 			var currenturl = document.location.href;
 			currenturl = currenturl.split("?");
 			currenturl = currenturl[0];
-			var refresh = currenturl + "?" + nextslide;
+			var refresh = currenturl + "?slide=" + nextslide + "&control=" + play_control;
+			alert(refresh);
 			window.location.href = refresh;			
 		});	
 
 		$('.PREV').click(function(){
 			images.eq(index).fadeOut(300);
-			index=(index-1) % images.length;
-			images.eq(index).delay(300).fadeIn(300);
-		});	
+			index=(index) % images.length;
+			var prevslide = index;
+			var currenturl = document.location.href;
+			currenturl = currenturl.split("?");
+			currenturl = currenturl[0];
+			var refresh = currenturl + "?slide=" + prevslide + "&control=" + play_control;
+			alert(refresh);
+			window.location.href = refresh;	
+		});
 
 
 });
